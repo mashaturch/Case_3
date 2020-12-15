c0f7603e37d8a8eb0d2087bcb9221a69fb98a53b
Tanuaaz <t.zubareva@g.nsu.ru>
2020-12-15 15:21:40 +0700

    begining

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 # Developers:   Kostylev M. (%),
 #               Turchinovich M. (%),
 #               Zubareva T. (%).
-# This program allows to calculate annual tax of the subject of taxation on American system.
+# This program allows to calculate the amount of capital on the bank deposit after a certain number of years
 
-year = int (input ("Введите срок размещения капитала (лет)"))
-capital_rate = int (input ("Введите начальный капитал ($)"))
-interest_rate = int (input ("Введите процентную ставку (%/мес.)))
-investment_injection = int (input ("Введите инвестиционные вливания ($/мес.))
+year = int (input ("Введите срок размещения капитала (лет) "))
+capital_rate = float (input ("Введите начальный капитал ($) "))
+interest_rate = int (input ("Введите процентную ставку (%/мес.) "))
+investment_injection = int (input ("Введите инвестиционные вливания ($/мес.) "))
+i = 0
+month = 0
+while i < year:
+    i += 1
+    print(i, "год")
+    print("----------------------------------------------")
+    print("|         |   основа   |  сумма %  |          |")
+    print("|  месяц  | инвестиций | за месяц  | капитал  |")
+
+
+
+
+
