ee1e90beb644ad7b44036ad3ae62467ecbe45ca5
mashaturch <s1yguvzg>
2020-12-15 13:14:38 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Case-study #3
+# Developers:   Kostylev M. (%),
+#               Turchinovich M. (%),
+#               Zubareva T. (%).
+# This program allows to calculate annual tax of the subject of taxation on American system.
+
+year = int (input ())
+capital_rate = int (input ())
